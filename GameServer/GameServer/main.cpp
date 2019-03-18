c1f1c05bd16d87e8b06f379a96f67b8794895ebb
이우상 <lus0506@gmail.com>
2019-03-18 13:10:40 +0900

    게임서버 숙제 2번 커밋

--- GameServer/GameServer/main.cpp
+++ GameServer/GameServer/main.cpp
@@ -4,6 +4,7 @@ void err_quit(const char*);
 void err_display(const char*);
 int recvn(SOCKET, char*, int, int);
 bool Initialize();
+void Update(SOCKET& client_socket);
 void KeyDistribute(CS_MovePacket&);
 
 SC_MovePacket sc_packet;
@@ -14,7 +15,6 @@ int main()
 
 	if (!Initialize())
 		return 0;
-	
 }
 
 void err_quit(const char* msg)
@@ -86,7 +86,6 @@ bool Initialize()
 
 	// Socket( )
 	// 대기 소켓 생성
-	//SOCKET listen_socket = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, nullptr, , 0);
 	SOCKET listen_socket = socket(AF_INET, SOCK_STREAM, 0);
 
 	if (listen_socket == INVALID_SOCKET)
@@ -125,37 +124,19 @@ bool Initialize()
 
 	while (true)
 	{
+		// accept( )
 		addrlen = sizeof(client_addr);
 		client_socket = accept(listen_socket, (SOCKADDR*)&client_addr, &addrlen);
 		if (client_socket == INVALID_SOCKET)
 		{
 			err_display("accept( )");
-			return false;
+			break;
 		}
 		cout << "\n[ TCP 서버 ] 클라이언트 접속 - IP : " << inet_ntoa(client_addr.sin_addr)
 			<< ", 포트 번호 : " << ntohs(client_addr.sin_port) << endl;
 
-		while (true)
-		{
-			retval = send(client_socket, (char*)&sc_packet, sizeof(sc_packet), 0);
-			if (retval == SOCKET_ERROR)
-			{
-				err_display("send( )");
-				break;
-			}
-
-			CS_MovePacket cs_packet;
-			// 데이터 받기
-			retval = recvn(client_socket, (char*)&cs_packet, sizeof(cs_packet), 0);
-			if (retval == SOCKET_ERROR)
-			{
-				err_display("recvn( )");
-				break;
-			}
-			// 받은 데이터
-			KeyDistribute(cs_packet);
-		}
-		closesocket(client_socket);
+		Update(client_socket);
+		break;
 	}
 
 	closesocket(listen_socket);
@@ -163,6 +144,31 @@ bool Initialize()
 	WSACleanup();
 
 	return true;
+}
+
+void Update(SOCKET& client_socket)
+{
+	while (true)
+	{
+		int retval = send(client_socket, (char*)&sc_packet, sizeof(sc_packet), 0);
+		if (retval == SOCKET_ERROR)
+		{
+			err_display("send( )");
+			break;
+		}
+
+		CS_MovePacket cs_packet;
+		// 데이터 받기
+		retval = recvn(client_socket, (char*)&cs_packet, sizeof(cs_packet), 0);
+		if (retval == SOCKET_ERROR)
+		{
+			err_display("recvn( )");
+			break;
+		}
+		// 받은 데이터
+		KeyDistribute(cs_packet);
+	}
+	closesocket(client_socket);
 }
 
 void KeyDistribute(CS_MovePacket& key)
@@ -181,10 +187,8 @@ void KeyDistribute(CS_MovePacket& key)
 	case KEY_DOWN:
 		sc_packet.m_Y -= 100;
 		break;
-		// KETY_IDLE
+		// KEY_IDLE
 	default:
 		break;
 	}
-
-//	cout << sc_packet.m_X << ", " << sc_packet.m_Y << endl;
 }
